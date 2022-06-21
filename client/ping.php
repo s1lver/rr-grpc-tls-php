e019e1aabc338c93d131bc3af966f06c51aa9a2d
EAMoiseenko <s1lverhron@gmail.com>
2022-06-21 16:24:50 +0300

    Added support SSL

--- client/ping.php
+++ client/ping.php
@@ -1,11 +1,14 @@
 <?php
-
 declare(strict_types=1);
 
 require __DIR__.'/vendor/autoload.php';
 
 $client = new \EchoClient('192.168.231.2:9001', [
-    'credentials' => \Grpc\ChannelCredentials::createInsecure(),
+    'credentials' => \Grpc\ChannelCredentials::createSsl(
+        file_get_contents('/var/www/rootCA.pem'),
+        file_get_contents('/var/www/localhost+3-client-key.pem'),
+        file_get_contents('/var/www/localhost+3-client.pem')
+    ),
 ]);
 
 $message = new Service\Message();
